6c1facb6e3be0d068c76641271cc210111ae143e
Darrion Whitfield <dwhitfield@ramfield.net>
2024-05-10 12:51:47 -0500

    version

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // #![feature(try_trait_v2)]
 #![cfg_attr(rust_comp_feature = "try_trait_v2", feature(try_trait_v2))]
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub mod errors;
 #[deprecated(since = "0.1.0", note = "please use `custom_error` instead")]
 pub mod errors_dep;
